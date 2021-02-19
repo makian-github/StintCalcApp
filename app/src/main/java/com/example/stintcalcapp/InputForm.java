d95d453516b679f9f8f16966d4887d2a9f4d3783
makian <makian.gpc>
2021-02-20 01:27:06 +0900

    フォーム画面でDatePickerを出して設定できるところまで試作

--- app/src/main/java/com/example/stintcalcapp/InputForm.java
+++ app/src/main/java/com/example/stintcalcapp/InputForm.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,10 +15,13 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class InputForm extends AppCompatActivity {
+import java.util.Locale;
+
+public class InputForm extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
 
     private EditText EditDriverName;
     private StintData stintData;
+    private TextView timeText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class InputForm extends AppCompatActivity {
         Button setButton = findViewById(R.id.setButton);
         Button timeSetButton = findViewById(R.id.setButton1);
         EditDriverName = findViewById(R.id.edit_driverName);
+        timeText = findViewById(R.id.timeText);
 
         setButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +57,20 @@ public class InputForm extends AppCompatActivity {
                     fragmentTransaction.commit();
             }
         });
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        String str = String.format(Locale.US, "%d:%d", hourOfDay, minute);
+        timeText.setText( str );
+//        EditDriverName.setText(str);
+        stintData.setDriverName(str);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePick();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+
     }
 
 }
